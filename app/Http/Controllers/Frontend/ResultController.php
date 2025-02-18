c053820551c823a0cea76adfa2a19a54b97a467b
setiawanboedy <gonisetiawan0@gmail.com>
2025-02-18 15:30:54 +0800

    populate ctrl success

--- app/Http/Controllers/Frontend/ResultController.php
+++ app/Http/Controllers/Frontend/ResultController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Storage;
 
 class ResultController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('result');
+        $jsonPath = storage_path('app/public/data.json');
+        $jsonData = json_decode(file_get_contents($jsonPath), true);
+        
+        $results = collect($jsonData['results']);
+
+        $perPage = 10;
+        $currentPage = $request->query('page', 1);
+        $pageData = $results->slice(($currentPage - 1) * $perPage, $perPage)->all();
+
+        $paginatedResults = new LengthAwarePaginator(
+            $pageData,
+            $results->count(),
+            $perPage,
+            $currentPage,
+            ['path'=> url('/results')]
+        );
+        return view('result', compact('paginatedResults'));
     }
 }
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -29,39 +29,22 @@
     <section class="container mx-auto bg-white rounded-xl py-8 px-4 mt-10">
         <div class="flex items-center justify-center">
             <ul class="list-none space-y-2 flex flex-col">
-                <li class="items-start max-w-3xl w-full rounded-md hover:bg-gray-100 cursor-pointer">
-                    <a href="{{route('detail.index')}}" class="flex flex-col gap-1 p-4">
-                        <div class="font-semibold text-xl">Profil Keterampilan Proses Sains Mahasiswa Melalui Implementasi SPADA Unram</div>
-                        <div class="text-gray-600">2021 <span>.</span> Muhammad Syazali, Aisa Nikmah Rahmatih, et al. <span>.</span> <span class="italic">JOURNAL PIJAR MIPA</span></div>
-                        <div class="text-sm font-semibold flex items-center gap-4">
-                            <span>Cited by 25</span>
-                            <span class="text-blue-600 underline cursor-pointer">PDF</span>
-                        </div>
-                    </a>
-                </li>
-                <li class="items-start max-w-3xl w-full rounded-md hover:bg-gray-100 cursor-pointer">
-                    <a href="./detail.html" class="flex flex-col gap-1 p-4">
-                        <div class="font-semibold text-xl">Profil Keterampilan Proses Sains Mahasiswa Melalui Implementasi SPADA Unram</div>
-                        <div class="text-gray-600">2021 <span>.</span> Muhammad Syazali, Aisa Nikmah Rahmatih, et al. <span>.</span> <span class="italic">JOURNAL PIJAR MIPA</span></div>
-                        <div class="text-sm font-semibold flex items-center gap-4">
-                            <span>Cited by 25</span>
-                            <span class="text-blue-600 underline cursor-pointer">PDF</span>
-                        </div>
-                    </a>
-                </li>
-                <li class="items-start max-w-3xl w-full rounded-md hover:bg-gray-100 cursor-pointer">
-                    <a href="./detail.html" class="flex flex-col gap-1 p-4">
-                        <div class="font-semibold text-xl">Profil Keterampilan Proses Sains Mahasiswa Melalui Implementasi SPADA Unram</div>
-                        <div class="text-gray-600">2021 <span>.</span> Muhammad Syazali, Aisa Nikmah Rahmatih, et al. <span>.</span> <span class="italic">JOURNAL PIJAR MIPA</span></div>
-                        <div class="text-sm font-semibold flex items-center gap-4">
-                            <span>Cited by 25</span>
-                            <span class="text-blue-600 underline cursor-pointer">PDF</span>
-                        </div>
-                    </a>
-                </li>
-                
-                
+                @foreach ($paginatedResults as $result)
+                    <li class="items-start max-w-3xl w-full rounded-md hover:bg-gray-100 cursor-pointer">
+                        <a href="{{route('detail.index')}}" class="flex flex-col gap-1 p-4">
+                            <div class="font-semibold text-xl">{{$result['title']}}</div>
+                            <div class="text-gray-600">2021 <span>.</span> Muhammad Syazali, Aisa Nikmah Rahmatih, et al. <span>.</span> <span class="italic">JOURNAL PIJAR MIPA</span></div>
+                            <div class="text-sm font-semibold flex items-center gap-4">
+                                <span>Cited by 25</span>
+                                <span class="text-blue-600 underline cursor-pointer">PDF</span>
+                            </div>
+                        </a>
+                    </li>
+                @endforeach
             </ul>
+        </div>
+         <div class="mt-4 max-w-3xl w-full mx-auto px-4">
+            {{ $paginatedResults->links() }}
         </div>
     </section>
 @endsection
